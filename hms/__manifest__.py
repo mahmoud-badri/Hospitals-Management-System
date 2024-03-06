bcdd6610e68be30498fbca60baabcc26c263c0ff
mahmoud-badri <eng.mahmoud.badrii@gmail.com>
2024-03-06 03:05:22 +0200

    create patient model

---
+++ hms/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "HMS",
+    'author': "Mahmoud Badry",
+    'category': "",
+    'version': "17.0.0.1.0",
+    'depends': ["base",
+                ],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/patient_view.xml'
+    ],
+    'application': True,
+}
---
+++ hms/models/patient.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+class Patient(models.Model):
+    _name = "hms.patient"
+    _description = "Patient"
+    _rec_name = "f_name"
+
+    f_name = fields.Char("First name")
+    l_name = fields.Char("Last name")
+    birth_day = fields.Date("Birth date")
+    history = fields.Html()
+    cr_ratio = fields.Float("CR Ratio")
+    blood = fields.Selection([
+        ("type_a", "Type A"),
+        ("type_b", "Type B"),
+        ("type_ab", "Type AB"),
+        ("type_o", "Type O"),
+    ])
+    pcr = fields.Boolean('PCR')
+    image = fields.Binary()
+    address = fields.Text()
+    age = fields.Integer()
