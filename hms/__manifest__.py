c7a96fc28b717ed6762cf95edaa98eb759db4ad4
mahmoud-badri <eng.mahmoud.badrii@gmail.com>
2024-03-11 18:57:09 +0200

    add report

--- hms/__manifest__.py
+++ hms/__manifest__.py
@@ -6,12 +6,14 @@
     'depends': ["base", "crm",
                 ],
     'data':[
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/base_menu.xml',
         'views/patient_view.xml',
         'views/department.xml',
         'views/doctor.xml',
         'views/customers_crm.xml',
+        'reports/ patient_report.xml',
     ],
     'application': True,
 }
